5bf3f5e123b9ad44a5cb5d0974895fc6ee533c40
Gabriel Martins <omartinsgabriel@icloud.com>
2025-03-18 17:46:39 -0300

    Função para sortear um amigo aleatório

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -17,7 +17,7 @@ function adicionarAmigo (){
 
     //Validando se o nome já foi inserido anteriormente
     if(amigos.includes(nomeAmigo)){
-        alert("O nome ${nomeAmigo} já foi inserido na lista!");
+        alert(`O nome ${nomeAmigo} já foi inserido na lista!`);
         return;
     }
 
@@ -30,6 +30,43 @@ function adicionarAmigo (){
     //Atualizar a lista no HTML
     atualizarLista();
 
+}
+
+//Função para atualizar a lista de amigos na interface
+function atualizarLista(){
+    const listaAmigos = document.getElementById('listaAmigos')
+
+    //Limpar o conteúdo atual da Lista
+    listaAmigos.innerHTML = "";
+
+    //Percorrer pelo array através do for
+    for(let i = 0; i <amigos.length; i++){
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
+
+
+//Função para sortear um amigo aleatório
+function sortearAmigo(){
+    //Validar que haja amigos disponíveis
+    if (amigos.length == 0){
+        alert("Não há amigos disponíveis para sortear. Adicione todos os amigos e refaça o sorteio.");
+        return;
+
+    }
+
+
+    //Gerando um indíce aleatório
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    //Obtendo um número sorteado
+    const amigoSorteado = amigos[indiceAleatorio]; //Usa o indíce aleatório para obter um nome do array.
+
+    //Mostrando o resultado no HTML
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Amigo Sorteado: <strong>${amigosorteado}</strong>`;
 }
 
 
